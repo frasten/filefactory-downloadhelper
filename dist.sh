7e7dca0cd4f84a77233a96d174179b7c843e52a6
Andrea Piccinelli <frasten@gmail.com>
2010-02-17 16:33:49 +0100

    Don't include .gitignore in the dist .zip file.
    Signed-off-by: Andrea Piccinelli <frasten@gmail.com>

--- dist.sh
+++ dist.sh
@@ -3,5 +3,6 @@ VER=`git describe --tags`
 DISTDIR=filefactory-downloadhelper-$VER
 mkdir -p $DISTDIR
 git archive HEAD | tar -x -C $DISTDIR
+rm -f $DISTDIR/.gitignore
 zip -r $DISTDIR $DISTDIR
 rm -rf $DISTDIR
