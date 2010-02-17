2e24c1529eb64cc85614745fa7a15de38d8021f4
Andrea Piccinelli <frasten@gmail.com>
2010-02-17 16:38:52 +0100

    Forgot a bug in the zip command.
    Signed-off-by: Andrea Piccinelli <frasten@gmail.com>

--- dist.sh
+++ dist.sh
@@ -4,7 +4,7 @@ DISTDIR=filefactory-downloadhelper-$VER
 mkdir -p $DISTDIR
 git archive HEAD | tar -x -C $DISTDIR
 rm -f $DISTDIR/.gitignore $DISTDIR/dist.sh
-zip -r $DISTDIR $DISTDIR
+zip -r $DISTDIR.zip $DISTDIR
 rm -rf $DISTDIR
 echo 
 echo "-----------------------------------------------------------------"
