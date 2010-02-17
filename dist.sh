cba48ed9160a59f3aebd486dbc095d8ebc8cb0f2
Andrea Piccinelli <frasten@gmail.com>
2010-02-17 16:34:35 +0100

    Don't include dist.sh script in the dist package.
    Signed-off-by: Andrea Piccinelli <frasten@gmail.com>

--- dist.sh
+++ dist.sh
@@ -3,6 +3,6 @@ VER=`git describe --tags`
 DISTDIR=filefactory-downloadhelper-$VER
 mkdir -p $DISTDIR
 git archive HEAD | tar -x -C $DISTDIR
-rm -f $DISTDIR/.gitignore
+rm -f $DISTDIR/.gitignore $DISTDIR/dist.sh
 zip -r $DISTDIR $DISTDIR
 rm -rf $DISTDIR
