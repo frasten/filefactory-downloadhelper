f598b6a9240e415f5695730654f28424b190011d
Andrea Piccinelli <frasten@gmail.com>
2010-02-17 16:36:07 +0100

    Added some descriptions in the script file.
    Signed-off-by: Andrea Piccinelli <frasten@gmail.com>

--- dist.sh
+++ dist.sh
@@ -6,3 +6,8 @@ git archive HEAD | tar -x -C $DISTDIR
 rm -f $DISTDIR/.gitignore $DISTDIR/dist.sh
 zip -r $DISTDIR $DISTDIR
 rm -rf $DISTDIR
+echo 
+echo "-----------------------------------------------------------------"
+echo "$DISTDIR.zip file created. Now upload it to:"
+echo "https://chrome.google.com/extensions/developer/update"
+echo
